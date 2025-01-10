7a4ed3c18f592a6070c5ba4a13e66320a7a0befb
Vigneshvar pandi R <vigneshvarpandi@gmail.com>
2025-01-10 12:54:26 +0530

    operation

---
+++ Day5/operators.js
@@ -0,0 +1,38 @@
+// 1Logical Operator
+
+// var a = 5
+// var b = "5"
+// console.log(a == b)
+// console.log(a  == b);
+
+// var c = 10
+// var d = 2 
+// console.log(c&d);
+// console.log(c|d);
+// console.log(~c);
+// console.log(c^d);
+// console.log(d<<2);
+// console.log(c>>2);
+
+//6.spear operation
+// let a = [10,20,30];
+// let b = [50,60,70];
+// let c = [...a,b];
+// console.log(c);
+
+// function restOpEg(a, b,...arg) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(arg);
+// }
+// restOpEg(10, 20, 30, 40, 50, ...b);
+
+// 8. Destructing operation
+// var array = [10, 20, 30];
+// var [a, b, c, d] =array;
+// console.log(a, b, c, d);
+
+// 9. Hoisting
+console.log(a);
+var a = 10;
+console.log(a);
