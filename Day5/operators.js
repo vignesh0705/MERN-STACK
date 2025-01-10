826dab7ca22b82927416d6d6a75e139c569f9b5a
Vigneshvar pandi R <vigneshvarpandi@gmail.com>
2025-01-10 09:36:18 +0530

    Animation

---
+++ Day5/operators.js
@@ -0,0 +1,15 @@
+// Logical Operator
+
+// var a = 5
+// var b = "5"
+// console.log(a == b)
+// console.log(a  == b);
+
+var c = 10
+var d = 2 
+console.log(c&d);
+console.log(c|d);
+console.log(~c);
+console.log(c^d);
+console.log(d<<2);
+console.log(c>>2);
