ee9d06b65bd796def4e8c3365a588759810f2cda
Vigneshvar pandi R <vigneshvarpandi@gmail.com>
2025-01-10 08:51:18 +0530

    java Script

--- parc.js
+++ parc.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+// console.log("Hello");
+
+a = 1;
+b = 'a';
+c = 'String';
+console.log(a);
+console.log(b);
+console.log(c);
+
+obj = {FirstName:"Sri",LastName:"Eshwar"};
+console.log(obj);
+
+obj1 ={};
+obj1.count = 30;
+console.log(obj1);
+
+obj2= new Object();
+obj2.count = 70;
+obj2.count = 1;
+console.log(obj2);
+
+set = new set {"hello",2,"SECE",4};
+console.log(set);
