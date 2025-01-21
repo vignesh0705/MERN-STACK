c92a3430ba4c3cb4c54c0648d1f981f96ef3f129
Vigneshvar pandi R <vigneshvarpandi@gmail.com>
2025-01-21 12:29:30 +0530

    project

--- SECE_Frontend/src/components/FunctionalComponents/About.jsx
+++ SECE_Frontend/src/components/FunctionalComponents/About.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 const About = () => {
+    var {text, settext} = useState("vicky")
     return(
         <div>
             <h2>This is About Component.</h2>
+            <h2>Textarea:</h2>
+            <textarea value={text} onChange={(event)=>settext(event.target,value)}></textarea>
         </div>
     )
 }
--- SECE_Frontend/src/components/FunctionalComponents/Gallery.jsx
+++ SECE_Frontend/src/components/FunctionalComponents/Gallery.jsx
@@ -2,7 +2,7 @@ const Gallery = (sece) => {
   return (
     <div>
       <h2>Welcome to {sece.page} page</h2>
-      <img src="D:\Mern stack\SECE_Frontend\src\assets\logo.png" alt="" />
+      <img src="src\assets\logo.png" alt="" />
       <h3>The image is {sece.img}</h3>
     </div>
   );
